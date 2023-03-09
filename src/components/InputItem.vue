1265059393e956b7da738498724fda9c7fd6c918
gongz <154129423@qq.com>
2023-03-09 21:39:18 +0800

    更新InputItem

--- src/components/InputItem.vue
+++ src/components/InputItem.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 
+defineProps<{
+  placeholder: string
+}>()
 </script>
 <template>
   <div class="item">
     <label>
       <slot name="label"></slot>
     </label>
-    <div class="input">
-      <input />
-    </div>
+    <input :placeholder="placeholder" />
     <small>
       <slot name="verify"></slot>
     </small>
@@ -17,9 +18,25 @@
 
 <style scoped>
 .item {
-
+  margin-bottom: 10px;
+}
+label {
+  color: #666666;
+  display: block;
+  margin-bottom: 5px;
 }
 small {
   color: #e74c3c;
+}
+input {
+  width: 100%;
+  height: 40px;
+  border-radius: 4px;
+  border: 2px solid #f0f0f0;
+  padding: 5px 10px;
+  outline-style: none;
+}
+input:focus {
+  border-color: #666666;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import InputItem from '../components/InputItem.vue'
+
 </script>
 
 <template>
   <div class="container">
     <div class="loginBox">
-      <h3>Register With Us</h3>
-      <InputItem>
+      <h2>Register With Us</h2>
+      <InputItem placeholder="Enter username">
         <template #label>
           Username
+        </template>
+        <template #verify>
+          Username must be at least 3 characters
+        </template>
+      </InputItem>
+      <InputItem placeholder="Enter email">
+        <template #label>
+          Email
+        </template>
+        <template #verify>
+          Email is not valid
         </template>
       </InputItem>
     </div>
@@ -26,6 +38,12 @@ import InputItem from '../components/InputItem.vue'
 .loginBox {
   width: 400px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, .3);
-  justify-content: center;
+  padding: 30px;
+}
+h2 {
+  text-align: center;
+  font-size: 1.5rem;
+  /* color: #000000; */
+  margin-bottom: 20px;
 }
 </style>
