d94dc63b6a4461e998b6bb68678cfd249bbfb685
gongzhan <gongzhan@apexsoft.com.cn>
2023-03-15 16:46:19 +0800

    路由自动加载

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,7 +3,7 @@ export function loadAllPlugins(app: any) {
   const keys = Object.keys(files);
   keys.forEach(async key => {
     const file: any = await files[key]()
-    console.log('====> files', file.default)
+    // console.log('====> files', file.default)
     if (typeof file.default === 'function') {
       if (key !== './index.ts') file.default(app)
     }
